053a70f04ec9363c28bb72ce87cbffa5c218a3e7
Ambili-Joseph <69898320+Ambili-Joseph@users.noreply.github.com>
2025-09-09 15:33:39 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 route::prefix('v1')->group(function(){
-    route::get('handset',[App\Http\Controllers\Api\v1\handsetController::class,'index']);
-    route::get('handset/{id}',[App\Http\Controllers\Api\v1\handsetController::class,'show']);
+    route::get('handsets',[App\Http\Controllers\Api\v1\handsetController::class,'index']);
+    route::get('handsets/{id}',[App\Http\Controllers\Api\v1\handsetController::class,'show']);
 
 
 });
